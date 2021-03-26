2984b2f72d99803fdff5c59a28b58cbcf905ff9b
ratistiss <50683985+ratistiss@users.noreply.github.com>
2021-03-26 12:14:05 -0400

    cleanup and form file linked

--- race_tracker/src/Components/Router.js
+++ race_tracker/src/Components/Router.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, Switch} from 'react-router-dom';
 import Standings from './Standings'
+import Form from './Form'
 
 
 const Router = () => {
     return (
         <Switch>
             <Route exact path="/" component={Standings} />
-            <Route exact path="/AddResults"/>
+            <Route exact path="/AddResults" component={Form}/>
             <Route path='*'/>
         </Switch>
     )
